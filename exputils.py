099988467e7f7fbe87cedbd78cdbced7cb1e8691
mmagnuski <mmagnuski@swps.edu.pl>
2016-02-14 22:22:09 +0100

    add stuff from TurboSternberg, fix bugs

--- exputils.py
+++ exputils.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function
+
+import os
+import six
+import yaml
 import random
 import numpy as np
 import pandas as pd
@@ -21,8 +27,33 @@ class LinOrdExperiment(object):
 	def __init__(self, window, config_file):
 		# super class should do this:
 		self.window = window
-		self.times = settings['times']
-		# turn to frames
+		self.frame_time = get_frame_time(window)
+
+		# load settings
+		file_name = os.path.join(os.getcwd(), config_file)
+		with open(file_name, 'r') as f:
+		    settings = yaml.load(f)
+	
+		self.times = s2frames(settings['times'], self.frame_time)
+		self.settings = settings
+
+		self.resp_keys = settings['resp_keys']
+		rnd = random.sample([True, False], 1)[0]
+		self.resp_mapping = {self.resp_keys[0]: rnd}
+		self.resp_mapping.update({self.resp_keys[1]: not rnd})
+
+		self.quitopt = settings['quit']
+		if self.quitopt['enable']:
+			self.resp_keys.append(self.quitopt['button'])
+
+		# dataframe
+		# self.df = create_empty_df(len(self.loads) * self.trials_per_load)
+		# self.df = self.df.set_index('trial', drop=True)
+
+		self.clock = core.Clock()
+		self.current_trial = 0
+
+		self.subject_id = 'test_subject'
 
 		self.letters = list('bcdfghjklmnprstwxz')
 		self.relations = ['>', '<']
@@ -39,14 +70,18 @@ class LinOrdExperiment(object):
 	def get_time(self, stim):
 		time = self.times[stim]
 		if isinstance(time, list):
-			return random.randint(**time)
-		else
+			return random.randint(*time)
+		else:
 			return time
 
 	def create_stimuli(self):
-		args = {'units': 'deg', 'height': self.settings['letter_height']}
+		args = {'units': 'deg', 'height': self.settings['sizes']['letter']}
 		self.stim = {l: visual.TextStim(self.window, text=l, **args)
-			for l in self.letters + self.relations}
+			for l in self.letters + self.relations + ['?']}
+
+		# fixation cross/circle
+		self.stim['fix'] = fix(self.window, height=self.settings['sizes']['fix_height'], 
+			width=self.settings['sizes']['fix_width'], shape=self.settings['fix_shape'])
 		if self.settings['fix_between_pairs']:
 			self.stim.update(btw_pairs=self.stim['fix'])
 		if self.settings['fix_during_wait']:
@@ -54,7 +89,11 @@ class LinOrdExperiment(object):
 
 	def _show_pair(self, pair, times):
 		# show_pair can return randomized times
-		elems = pair.split('')
+		# elems = pair.split('')
+		elems = list(pair)
+		elems = [x.lower() for x in elems]
+		assert len(elems) == len(times)
+
 		[self.show_element(el, tm) for el, tm in zip(elems, times)]
 
 	def show_element(self, elem, time):
@@ -75,14 +114,33 @@ class LinOrdExperiment(object):
 
 	def ask_question(self, question):
 		# show relation
-		# wait for response
+		times = self.show_pair(question)
+		self.clock.reset()
+		q_times = map(self.get_time, ['pre_question_mark', 'pre_question_mark'])
+		[self.show_element(el, tm) for el, tm in zip(['', '?'], q_times)]
+
+		resp = event.waitKeys(maxWait=self.times['response'],
+							  keyList=self.resp_keys,
+					  		  timeStamped=self.clock)
 		# return response
+		return (times, resp)
+
+	def ask_questions(self, model, relation):
+		questions = self.create_questions(model, relation)
+		output = list()
+		for q in questions:
+			# pre-question time?
+			output.append(self.ask_question(question))
 
 	def show_premises(self, model, sequence, relation):
 		all_times = list()
+		if isinstance(model, str):
+			model = list(model)
+		model = np.asarray(model)
+		sequence = np.asarray(sequence)
 		for i in [[0,1], [2,3], [4,5]]:
 			pair = model[sequence[i]]
-			pair = ' '.join(pair[0], relation, pair[1])
+			pair = ' '.join([pair[0], relation, pair[1]])
 			times = self.show_pair(pair)
 			if i is not [4,5]:
 				next_time = self.get_time('after_pair')
@@ -106,8 +164,54 @@ class LinOrdExperiment(object):
 		ind = random.sample(range(4), 1) + random.sample(range(2), 1)
 		current_cond = conditions[ind[0], ind[1], :]
 
+	def create_questions(self, model, relation):
+		if isinstance(model, str):
+			model = list(model)
+		model = np.asarray(model)
+		all_questions = [[[0,3]], [[0,2], [1,3]], [[0,1], [1,2], [2,3]]]
+		chosen_questions = [random.sample(x, 1)[0] for x in all_questions]
+		chosen_questions = [' '.join([model[x[0]], relation, model[x[1]]])
+			for x in chosen_questions]
+		random.shuffle(chosen_questions)
+		return chosen_questions
+
+
+# stimuli
+# -------
+def fix(win, height=0.3, width=0.1, shape='circle', color=(0.5, 0.5, 0.5)):
+	args = {'fillColor': color, 'lineColor': color,
+		'interpolate': True, 'units': 'deg'}
+	if shape == 'circle':
+		fix_stim = visual.Circle(win, radius=height/2,
+			edges=32, **args)
+	else:
+		h, w = (height/2, width/2)
+		vert = np.array([[w, -h], [w, h], [-w, h], [-w, -h]])
+
+		args.update(closeShape=True)
+		fix_stim = [visual.ShapeStim(win, vertices=v, **args)
+					for v in [vert, np.fliplr(vert)]]
+	return fix_stim
+
+# time
+# ----
+def get_frame_time(win, frames=25):
+	frame_rate = win.getActualFrameRate(nIdentical = frames)
+	return 1.0 / frame_rate
+
+def s2frames(time_in_seconds, frame_time):
+	assert isinstance(time_in_seconds, dict)
+	time_in_frames = dict()
+	toframes = lambda x: int(round(x / frame_time))
+	for k, v in six.iteritems(time_in_seconds):
+		if isinstance(v, list):
+			time_in_frames[k] = map(toframes, v)
+		else:
+			time_in_frames[k] = toframes(v)
+	return time_in_frames
+
 # wypisz kolejne pary
-relation = random.sample(['<', '>'], 1)[0]
-for i in [[0,1], [2,3], [4,5]]:
-	pair = sequence[current_cond[i]]
-	print(pair[0], relation, pair[1])
+# relation = random.sample(['<', '>'], 1)[0]
+# for i in [[0,1], [2,3], [4,5]]:
+# 	pair = sequence[current_cond[i]]
+# 	print(pair[0], relation, pair[1])

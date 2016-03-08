a54833b47a576d56f02b1896288dd35226fda164
mmagnuski <mmagnuski@swps.edu.pl>
2016-03-08 15:55:16 +0100

    upd tests

--- test_qst.py
+++ test_qst.py
@@ -3,17 +3,14 @@ from exputils import LinOrdExperiment
 from psychopy import visual
 
 os.chdir(r'D:\proj\LinOrd')
-win = visual.Window('testMonitor')
+win = visual.Window(monitor='testMonitor')
 exp = LinOrdExperiment(win, 'settings.yaml')
 df = exp.create_trials()
 df.head(10)
 
-qst = exp.get_questions('ABCD', '<', 1)
-for q in qst:
-	print(q)
-exp.trials.head(10)
-
-qst = exp.get_questions('ABCD', '<', 2)
-for q in qst:
-	print(q)
-exp.trials.head(10)
+for i in range(3):
+	qst = exp.get_questions('ABCD', '<', i)
+	for q in qst:
+		print(q)
+	exp.df.head(10)
+	exp.trials.head(10)

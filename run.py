0462ed916ad7d69b3712eabaa624cbfae09f9971
mmagnuski <mmagnuski@swps.edu.pl>
2016-05-16 17:50:48 +0200

    change run into fun for better importing and glueing with other procs

--- run.py
+++ run.py
@@ -6,66 +6,81 @@ import random
 from psychopy import core, visual, monitors, event
 from exputils import LinOrdExperiment, Instructions
 
-participantDistance = 60
 
-# set path
-file_path = os.path.join(*(__file__.split('\\')[:-1]))
-file_path = file_path.replace(':', ':\\')
-os.chdir(file_path)
+# how far is participant from the screen?
+scr_dist = 60
 
-# check correct monitor type
-monitorList = monitors.getAllMonitors()
-if 'BENQ-XL2411' in monitorList:
-    monitor = monitors.Monitor('BENQ-XL2411', width=53., 
-        distance=participantDistance)
-    monitor.setSizePix([1920, 1080])
-else:
-    monitor = 'testMonitor'
+def get_screen(scr_dist=scr_dist):
+    # check correct monitor type
+    monitorList = monitors.getAllMonitors()
+    if 'BENQ-XL2411' in monitorList:
+        monitor = monitors.Monitor('BENQ-XL2411', width=53.,
+            distance=scr_dist)
+        monitor.setSizePix([1920, 1080])
+    else:
+        monitor = 'testMonitor'
+    return monitor
 
-# create temporary window
-window = visual.Window(monitor=monitor, units="deg",
-    fullscr=False, size=[1200,800])
 
-exp = LinOrdExperiment(window, os.path.join(file_path, 'settings.yaml'))
-exp.get_subject_id()
+def run(window=None, subject_id=None, true_key=None,
+        scr_dist=scr_dist):
 
-#create fullscreen window
-window = visual.Window(monitor=monitor, units="deg", fullscr=True)
-waitText = visual.TextStim(window, text=u'Proszę czekać...', height=2)
-exp.set_window(window)
-waitText.draw(); window.flip()
-# hide mouse
-window.setMouseVisible(False)
+    # set path to current file location
+    file_path = os.path.join(*(__file__.split('\\')[:-1]))
+    file_path = file_path.replace(':', ':\\')
+    os.chdir(file_path)
 
-# at least now:
-exp.set_resp(true_key='f')
+    # create temporary window
+    monitor = get_screen(scr_dist=scr_dist)
+    temp_window = visual.Window(monitor=monitor, units="deg",
+        fullscr=False, size=[1200,800])
 
-# instructions
-instr_dir = os.path.join(os.getcwd(), 'instr')
-instr = os.listdir(instr_dir)
+    exp = LinOrdExperiment(temp_window, os.path.join(file_path, 'settings.yaml'))
+    exp.set_subject_id(subject_id)
+    exp.set_resp(true_key=true_key)
 
-if exp.resp_mapping['f']:
-    del instr[2]
-else:
-    del instr[1]
-instr = [os.path.join(instr_dir, f) for f in instr]
+    #create fullscreen window
+    if window is None:
+        window = visual.Window(monitor=monitor, units="deg", fullscr=True)
+    waitText = visual.TextStim(window, text=u'Proszę czekać...', height=2)
+    exp.set_window(window)
+    waitText.draw(); window.flip()
 
-instr = Instructions(window, instr)
-instr.present(stop=4)
+    # hide mouse
+    window.setMouseVisible(False)
 
-orig_subj_id = exp.subject['id']
-exp.subject['id'] += '_training'
-# training
-for i in range(1, 8):
-    exp.show_trial(i, feedback=True)
-    exp.save_data()
-    if i > 1 and exp.df.loc[i, 'ifcorrect'] == 0:
-        exp.show_keymap()
 
-exp.subject['id'] = orig_subj_id
-exp.create_trials(repetitions=exp.settings['repetitions'])
+    # instructions
+    instr_dir = os.path.join(os.getcwd(), 'instr')
+    instr = os.listdir(instr_dir)
 
-instr.present(stop=5)
+    if exp.resp_mapping['f']:
+        del instr[2]
+    else:
+        del instr[1]
+    instr = [os.path.join(instr_dir, f) for f in instr]
 
-exp.show_all_trials()
-instr.present()
+    instr = Instructions(window, instr)
+    instr.present(stop=4)
+
+    orig_subj_id = exp.subject['id']
+    exp.subject['id'] += '_training'
+    # training
+    for i in range(1, 8):
+        exp.show_trial(i, feedback=True)
+        exp.save_data()
+        if i > 1 and exp.df.loc[i, 'ifcorrect'] == 0:
+            exp.show_keymap()
+
+    exp.subject['id'] = orig_subj_id
+    exp.create_trials(repetitions=exp.settings['repetitions'])
+
+    instr.present(stop=5)
+
+    exp.show_all_trials()
+    instr.present()
+    return exp
+
+
+if __name__ == '__main__':
+    run()
